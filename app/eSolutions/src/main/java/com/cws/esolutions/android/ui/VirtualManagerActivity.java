345e13c5ebd6b56fff459a0640093293824e1b23
cws-khuntly <kmhuntly@gmail.com>
2014-02-05 15:49:54 -0500

    update

--- app/eSolutions/src/main/java/com/cws/esolutions/android/ui/VirtualManagerActivity.java
+++ app/eSolutions/src/main/java/com/cws/esolutions/android/ui/VirtualManagerActivity.java
@@ -26,24 +26,17 @@ package com.cws.esolutions.android.ui;
  * kmhuntly@gmail.com   11/23/2008 22:39:20             Created.
  */
 import java.util.List;
-
 import org.slf4j.Logger;
-
 import android.view.View;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.MenuItem;
 import android.content.Intent;
 import android.widget.TextView;
-
 import org.slf4j.LoggerFactory;
-
 import android.widget.RelativeLayout;
-
 import java.util.concurrent.TimeUnit;
-
 import android.view.View.OnClickListener;
-
 import java.util.concurrent.TimeoutException;
 import java.util.concurrent.ExecutionException;
 
