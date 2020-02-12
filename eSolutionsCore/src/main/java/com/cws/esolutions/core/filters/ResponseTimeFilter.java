9df118d74a1f7a0de9e4e8deff3783b108ba8adf
Kevin Huntly <kmhuntly@gmail.com>
2020-02-12 14:20:23 -0500

    summary (required)

--- eSolutionsCore/src/main/java/com/cws/esolutions/core/filters/ResponseTimeFilter.java
+++ eSolutionsCore/src/main/java/com/cws/esolutions/core/filters/ResponseTimeFilter.java
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2009 - 2020 CaspersBox Web Services
  * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
