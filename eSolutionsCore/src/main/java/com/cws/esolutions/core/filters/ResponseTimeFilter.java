8429edf51bf99518798819a5adca0cf00abd2a71
Kevin Huntly <kmhuntly@gmail.com>
2020-02-12 13:17:39 -0500

    update

---
+++ eSolutionsCore/src/main/java/com/cws/esolutions/core/filters/ResponseTimeFilter.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright (c) 2009 - 2020 CaspersBox Web Services
+ * 
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.cws.esolutions.core.filters;
+/*
+ * Project: eSolutionsCore
+ * Package: com.cws.esolutions.core.filters
+ * File: ResponseTimeFilter.java
+ *
+ * History
+ *
+ * Author               Date                            Comments
+ * ----------------------------------------------------------------------------
+ * cws-khuntly          11/23/2008 22:39:20             Created.
+ */
+import org.slf4j.Logger;
+import java.io.IOException;
+import javax.servlet.Filter;
+import org.slf4j.LoggerFactory;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import org.apache.commons.lang.StringUtils;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.http.HttpServletRequest;
+
+import com.cws.esolutions.core.CoreServicesConstants;
+/**
+ * @author cws-khuntly
+ * @version 1.0
+ * @see javax.servlet.Filter
+ */
+@WebFilter(filterName = "ResponseTimeFilter", urlPatterns = {"/*"}, initParams = @WebInitParam(name = "env", value = "dev"))
+public class ResponseTimeFilter implements Filter
+{
+    private FilterConfig filterConfig = null;
+
+    private static final String CNAME = ResponseTimeFilter.class.getName();
+    private static final Logger DEBUGGER = LoggerFactory.getLogger(CoreServicesConstants.DEBUGGER);
+    private static final boolean DEBUG = DEBUGGER.isDebugEnabled();
+
+    public void init(final FilterConfig config) throws ServletException
+    {
+    	final String methodName = ResponseTimeFilter.CNAME + "#init(final FilterConfig config) throws ServletException";
+
+    	if (DEBUG)
+    	{
+    		DEBUGGER.debug("Value: {}", methodName);
+    		DEBUGGER.debug("Value: {}", config);
+    	}
+
+        this.filterConfig = config;
+    }
+
+    public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain) throws IOException, ServletException
+    {
+    	final String methodName = ResponseTimeFilter.CNAME + "#doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain) throws IOException, ServletException";
+
+    	if (DEBUG)
+    	{
+    		DEBUGGER.debug("Value: {}", methodName);
+    		DEBUGGER.debug("Value: {}", request);
+    		DEBUGGER.debug("Value: {}", response);
+    		DEBUGGER.debug("Value: {}", chain);
+    	}
+
+        if (StringUtils.equals(filterConfig.getInitParameter("env"), "dev"))
+        {
+            long time = System.currentTimeMillis();
+
+            if (DEBUG)
+            {
+            	DEBUGGER.debug("Value: {}", time);
+            }
+
+            time = System.currentTimeMillis() - time;
+            String url = request instanceof HttpServletRequest ? ((HttpServletRequest) request).getRequestURL().toString() : "N/A";
+
+            if (DEBUG)
+            {
+            	DEBUGGER.debug("Value: {}", time);
+            	DEBUGGER.debug("Value: {}", url);
+            }
+
+            System.out.println("Time taken for request to complete:  " + time + "ms");
+            System.out.println("Request url : " + url);
+
+            chain.doFilter(request, response);
+        }
+        else
+        {
+            chain.doFilter(request, response);
+        }
+    }
+
+    public void destroy()
+    {
+    }
+}
